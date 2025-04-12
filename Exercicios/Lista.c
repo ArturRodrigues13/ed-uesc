65dae70c414b2531d030e7802d0e7a83d28e8edb
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-04-11 21:18:43 -0300

    Comenta Funções

--- Exercicios/Lista.c
+++ Exercicios/Lista.c
@@ -48,6 +48,7 @@
    void imprimiLista(Lista *lista);
 
 */
+
 typedef struct {
     int elementos[CAPACIDADE];
     int tamanho;
@@ -74,7 +75,6 @@ int estaCheia(Lista *lista) {
    Entrada: uma Lista e um elemento inteiro;
    Saída: 1 se o elemento foi inserido na Lista e 0 caso contrário (lista sem espaço).
 */
-
 int adicionaElemento(Lista *lista, int elemento) {
     if (estaCheia(lista)) {
         return 0; // Falha ao adicionar elemento
@@ -84,6 +84,10 @@ int adicionaElemento(Lista *lista, int elemento) {
     return 1; // Sucesso
 }
 
+/* Função do Menu para o usuário indicar qual elemento será adicionado na lista.
+   Entrada: uma Lista e um inteiro que armazenará o elemento que será adicionado na lista;
+   Saída: chamada da função adicionaElemento com a lista e o elemento anteriormente definidos como argumentos.
+*/
 int inserirItem(Lista *lista, int elemento) {
 	printf("\nPerfeito, me diga o elemento que será adicionado na Lista: ");
 	scanf("%d",&elemento);
@@ -105,6 +109,10 @@ int removeElemento(Lista *lista, int indice) {
     return 1; // Sucesso
 }
 
+/* Função do Menu para o usuário indicar qual índice será removido da lista.
+   Entrada: uma Lista e um inteiro que armazenará o índice que será removido da lista;
+   Saída: chamada da função removeElemento com a lista e o índice anteriormente definidos como argumentos.
+*/
 int removerItem(Lista *lista, int indice) {
 	printf("\nPerfeito, me diga o índice que será removido da Lista: ");
 	scanf("%d",&indice);
@@ -124,28 +132,42 @@ void imprimiLista(Lista *lista) {
     printf("\n");
 }
 
+/* Função do Menu que traz um pouco mais de legibilidade ao código.
+   Entrada: uma Lista;
+   Saída: chamada da função imprimiLista com a lista como argumento.
+*/
 void exibirLista(Lista *lista) {
 	printf("\nAqui está sua lista atual:\n");
 	imprimiLista(lista);
 }
 
+/* Função para pesquisar um elemento na lista
+   Entrada: uma Lista e o elemento que se deseja procurar.
+   Saída: o índice do elemento se ele foi encontradp com sucesso ou -1 caso contrário.
+*/
 int pesquisarElemento(Lista *lista, int elemento) {
 	for ( int i = 0 ; i < lista->tamanho ; i++ ) {
 		if (elemento == lista->elementos[i]) {
-			return i;
+			return i; // Sucesso
 		}
 	}
-	return -1;
+	return -1; // Elemento não presente na lista.
 }
 
+/* Função do Menu para o usuário indicar qual elemento ele deseja pesquisar na lista.
+   Entrada: uma Lista e um inteiro que armazenará o elemento que terá sua presença verificada na lista;
+   Saída: chamada da função pesquisarElemento com a lista e o elemento anteriormente definidos como argumentos.
+*/
 int pesquisarItem(Lista *lista, int elemento) {
 	printf("\nPerfeito, me diga qual elemento você deseja pesquisar na Lista: ");
 	scanf("%d",&elemento);
 	return pesquisarElemento(lista, elemento);
 }
 
-
-
+/* Função para exibir erros que o usuário pode ter ao chamar as funções.
+   Entrada: um inteiro que define qual erro ocorreu;
+   Saída: impressão de uma mensagem de feedback que muda conforme qual erro foi recebido.
+*/
 void printarErro(int indiceErro) {
 	if(indiceErro == 0) {
 		printf("\nNão há espaço para novo item.\nCAPACIDADE MÁXIMA = %d\n",CAPACIDADE);
@@ -156,6 +178,10 @@ void printarErro(int indiceErro) {
 	}
 }
 
+/* Função para ordenar um vetor de forma Crescente.
+   Entrada: um vetor, o seu ínicio, o índice do meio e o seu último ínidice, todos inteiros;
+   Saída: vetor ordenado de forma Crescente.
+*/
 void intercala(int *vetor, int inicio, int meio, int final) {
 
 	int auxVet[final];
@@ -184,6 +210,10 @@ void intercala(int *vetor, int inicio, int meio, int final) {
 	}
 }
 
+/* Função para ordenar um vetor de forma Crescente.
+   Entrada: um vetor, o seu ínicio e o seu último ínidice, todos inteiros;
+   Saída: vetor ordenado de forma Crescente.
+*/
 void mergeSort(int *vetor, int inicio, int final) {
 
 	if (inicio >= final)
