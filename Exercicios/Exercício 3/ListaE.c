d1d0461567a71cfd895281aca50f8df22dec192a
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-04-16 21:19:07 -0300

    Exercício 3 início

---
+++ Exercicios/Exercício 3/ListaE.c
@@ -0,0 +1,98 @@
+/********************************************************************
+ * Este arquivo cont�m c�digo para manipular uma lista encadeada de itens.
+ * Um item � uma estrutura definida pelo usu�rio da biblioteca ListaE.h.
+ * Uma lista encadeada � um ponteiro para um n�. Um n� possui dois campos:
+ * a) um campo do tipo tpItem;
+ * b) um ponteiro para o pr�ximo n�;
+ *
+ * Obs:
+ * -> O tipo tpItem deve conter obrigat�riamente um campo nomeado de id do tipo unsigned int.
+ *
+ */
+#include "ListaE.h"
+//-----------------------------------------------------------------------------
+struct Celula {
+       tpItem    item;
+       struct Celula  *prox;
+};
+
+typedef  struct Celula tpNo;
+//-----------------------------------------------------------------------------
+/**
+ * @param .
+ * @return a lista inicializada com o ponteiro apontando para NULL;
+ */
+tpNo * initLstE(){
+    tpNo *lst = NULL;
+    return lst;
+}
+
+/**
+ * @param lst = uma Lista de itens;
+ * @param item= um item a ser inserido em lst
+ * @return a lista se o item foi inserido em lst ou NULL caso contr�rio.
+ */
+tpNo *insertLstE(tpNo *lst, tpItem item){
+    tpNo *no = (tpNo*)malloc(sizeof(tpNo));
+   	if (no !=NULL){
+		no->item = item;
+		no->prox = lst;
+   		lst=no;
+   		return lst;
+	}
+    return NULL;
+}
+//-----------------------------------------------------------------------------
+/**
+ * @param lst = uma Lista n�o vazia de itens;
+ * @return retorna o item da primeira posi��o de lst.
+ */
+tpItem  getE(tpNo *lst){
+       return lst->item;
+}
+
+
+//-----------------------------------------------------------------------------
+/**
+ * Exibe no monitor os itens da lista.
+ * @param lst = uma Lista de itens;
+ * @return
+ */
+void printLstE(tpNo *lst){
+    //tpNo *aux = lst;
+
+    while (lst != NULL) {
+            printItem(lst->item);
+            printf("\n----------------------------------");
+            lst = lst->prox;
+    }
+}
+
+//-----------------------------------------------------------------------------
+/**
+ * Verifica se a lista está vazia.
+ * @param lst = uma Lista de itens;
+ * @return true se lst vazia ou false caso contrário.
+ */
+booleano isEmptyLstE(tpNo *lst){
+    if (lst == NULL) return true;
+    return false;
+}
+
+//-----------------------------------------------------------------------------
+/**
+ * Verifica se a lista está vazia.
+ * @param lst = uma Lista de itens;
+ * @param id = � o identificador que será buscado em lst.
+ * @return o item na lista com o id ou NULL se não for encontrado.
+ */
+tpItem * searchItemLstE(tpNo *lst, unsigned int id){
+    if  (lst != NULL){
+        if (lst->item.id == id){
+            return &(lst->item);
+        } else {
+            return searchItemLstE(lst->prox, id);
+        }
+    }
+    return NULL;
+}
